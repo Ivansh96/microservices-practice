69c94af33135ccb6e8c1a7ebbd7bb6d0c2962a85
Ivansh96 <evgenijshavshin@yandex.ru>
2023-05-31 17:56:49 +0700

    -

---
+++ customer/src/main/java/com/testing/customer/dal/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.testing.customer.dal.repository;
+
+import com.testing.customer.dal.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
--- customer/src/main/java/com/testing/customer/service/CustomerService.java
+++ customer/src/main/java/com/testing/customer/service/CustomerService.java
@@ -1,11 +1,12 @@
 package com.testing.customer.service;
 
+import com.testing.customer.dal.repository.CustomerRepository;
 import com.testing.customer.dto.CustomerRequest;
-import com.testing.customer.model.Customer;
+import com.testing.customer.dal.model.Customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -13,6 +14,7 @@ public record CustomerService() {
                 .email(request.email())
                 .build();
         // todo: email validation
-        // todo: customer to db
+
+        customerRepository.save(customer);
     }
 }
